d960f9758de3c15fa621fdf661ad39d3b6abfe0d
Rob Gaskin <robert.gaskin@answerdigital.com>
2022-10-12 17:27:36 +0100

    QOL Fixes, README Changes and flip-app always provided

---
+++ docker/apps/flip-app/custom/utils/flip_constants.py
@@ -0,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+import os
+
+
+class FlipConstants(object):
+    CLEANUP: str = "cleanup"
+    MIN_CLIENTS: int = 1
+    INIT_TRAINING: str = "init_training"
+
+
+class FlipEvents(object):
+    TRAINING_INITIATED = "_training_initiated"
+    RESULTS_UPLOAD_STARTED = "_results_upload_started"
+    RESULTS_UPLOAD_COMPLETED = "_results_upload_completed"
+    ABORTED = "_aborted"
+
+
+class ModelStatus(str, Enum):
+    PENDING = "PENDING",
+    INITIATED = "INITIATED",
+    PREPARED = "PREPARED",
+    TRAINING_STARTED = "TRAINING_STARTED",
+    RESULTS_UPLOADED = "RESULTS_UPLOADED",
+    ERROR = "ERROR",
+    STOPPED = "STOPPED"
+
+class FlipMetricsLabel(str, Enum):
+    LOSS_FUNCTION = "LOSS_FUNCTION"
+    DL_RESULT = "DL_RESULT"
+    AVERAGE_SCORE = "AVERAGE_SCORE"
--- nvflare-poc-inacontainer/start_nvflare_components.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./poc/server/startup/start.sh &
-./poc/site-1/startup/start.sh &
-./poc/site-2/startup/start.sh &
-
-sleep infinity
