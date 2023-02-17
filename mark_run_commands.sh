5a9768fc10278f1b419a15e24c6ac7881a1ace48
Mark <mark.graham@kcl.ac.uk>
2023-02-17 16:33:40 +0000

    Run docker with GPU

--- mark_run_commands.sh
+++ mark_run_commands.sh
@@ -1,10 +1,9 @@
-docker run --rm -it -v /home/mark/projects/federated_learning/flip-sample-application/data:/data -v /home/mark/projects/federated_learning/flip-sample-application/apps/:/nvflare/poc/admin/transfer/ --entrypoint bash nvflare-in-one
+docker run --gpus all --rm -it -v /home/mark/projects/federated_learning/flip-sample-application/data:/data -v /home/mark/projects/federated_learning/flip-sample-application/apps/:/nvflare/poc/admin/transfer/  --shm-size=8gb --entrypoint bash nvflare-in-one
 
 bash start_nvflare_components.sh &
 ./poc/admin/startup/fl_admin.sh
 
-upload_app flip-app
+upload_app local-flip-app
 set_run_number 1
-deploy_app flip-app all
+deploy_app local-flip-app all
 start_app all
-
